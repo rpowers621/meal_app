313fcd4709b2efd6adfd67b44cc83a068eec1314
Ubuntu <rpowers8@RachelVirtual.liekm1wpnybezif1btbs0jesdh.bx.internal.cloudapp.net>
2021-11-03 15:51:20 +0000

    created search_rep_by_ingred function that takes in a string and returns a dictionay of recipe id  keys and recipes title values

--- spoonacular.py
+++ spoonacular.py
@@ -4,4 +4,33 @@ from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
-def get_access(): 
+API_KEY = os.getenv("API_KEY")
+
+
+def search_rep_by_ingred(str):
+
+    NUM_RECIPES = 5
+
+    BASE_URL = f"https://api.spoonacular.com/recipes/findByIngredients?apiKey={API_KEY}&ingredients={str}&number={NUM_RECIPES}"
+
+    headers = {"Content-Type": "application/json"}
+    # this is the number of recipes returned
+
+    response = requests.get(url=BASE_URL, headers=headers)
+    response_data = response.json()
+
+    recipe_ids = {}
+    length = len(response_data)
+
+    for i in range(length):
+        try:
+            recipe_ids[response_data[i]["id"]] = response_data[i]["title"]
+        except KeyError:
+            print(KeyError)
+            break
+
+    print(recipe_ids)
+
+
+ingreds = "apples,flour,sugar"
+search_rep_by_ingred(ingreds)
