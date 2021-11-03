244364573faf2524f406acc617ce667f99114291
Ubuntu <adithyazure@Slide-in-the-VM.tiv0r0se415upmft4042d5jvdc.bx.internal.cloudapp.net>
2021-11-03 15:10:15 +0000

    new commit

--- meal-planner-app/src/App.js
+++ meal-planner-app/src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function textInputForms(placeholder) {
+  <label>placeholder</label>
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Meal Planner</h1>
+      <form>
+      <label for="fname">Email:</label>
+      <input type="text" id="fname" name="fname"></input>
+      <label for="lname">Password:</label>
+      <input type="text" id="lname" name="lname"></input>
+      </form>
+      <button type="button">Login in</button>
+
+
     </div>
   );
 }
