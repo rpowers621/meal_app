0938c4e93b0142572524948078662a572fafba63
Jilal99 <ykim135@student.gsu.edu>
2021-11-13 00:33:44 +0000

    added dummy code to mealboard

---
+++ src/Mealboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Mealboard() {
+  
+  return (
+      <div className="Mealboard">
+        <h1>This is my Mealboard!</h1>
+
+        
+      </div>
+  );
+}
+
+export default Mealboard;
