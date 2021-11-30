3d1aa30cb8e91b97ba0beca692fe07a29740eb6c
melinaphan3 <77140751+melinaphan3@users.noreply.github.com>
2021-11-30 18:16:49 -0500

    changed everything from html to react

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
+import React from "react";
+import "./Home.css";
+import logo from '.logo.png';
+import bowl from '.bowl.png';
 
-<head>
-    <meta charset="UTF-8">
-    <title> The Culinary Army </title>
-    <link rel="stylesheet" href="Home.css">
-</head>
-
-<body>
-    <div class="circle"></div>
-    <section>
-        <header>
-            <a href="#" class="logo"><img src="logo.png"></a>
-            <ul>
-                <li><a href="#"> About Us </a></li>
-                <li><a href="#"> Contact </a></li>
-            </ul>
-        </header>
-        <div class="content">
-            <div class="textBox">
-                <h2> Meal Planning Made Easy</h2>
-                <p> Are you in need of a weekly meal plan but can't seem to find one that fits you? The Culinary Army
-                    allows you browse through tons of weekly meal plan ideas to find one that suits you or you can
-                    create your own delicious meals customized to your preference. The possibilities are endless! </p>
+function Homepage() {
+    return (
+        <div className="content">
+            <div className="image">
+                <img className="logo" src={logo} alt="logo"><</img>
+                <ul>
+                    <li><a href="#"> Mealboard </a></li>
+                    <li><a href="#"> About Us </a></li>
+                    <li><a href="#"> Contact </a></li>
+                </ul>
             </div>
-            <div class="imgBox">
-                <img src="meal.png" class="bowl">
+            <div className="Content">
+                <div className="text">
+                    <h2> Meal Planning Made Easy </h2>
+                    <p> Are you in need of a weekly meal plan but can't seem to find one that fits you? The Culinary Army allows you browse through tons of weekly meal plan ideas to find one that suits you or you can create your own delicious meals customized to your preference. The possibilities are endless!</p>
+                </div>
+                <div class="image2">
+                    <img className="bowl" src={bowl} alt="bowl"></img>
+                </div>
             </div>
         </div>
-    </section>
-</body>
-
-</html>
+    );
+}
