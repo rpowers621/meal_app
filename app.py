2110a945958f41ad93d6c07179c603883b632763
Jilal99 <ykim135@student.gsu.edu>
2021-12-01 16:14:03 +0000

    fixed css on about page

--- app.py
+++ app.py
@@ -533,5 +533,6 @@ app.register_blueprint(bp)
 
 if __name__ == "__main__":
     app.run(
-        host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True
+        #host=os.getenv("IP", "0.0.0.0"), 
+        port=int(os.getenv("PORT", 8080)), debug=True
     )
