fc38d3cadebf4ae9614d34bafd62ea80f5f93d77
Jilal99 <ykim135@student.gsu.edu>
2021-12-01 16:21:06 +0000

    fixed heroku environment

--- app.py
+++ app.py
@@ -533,6 +533,6 @@ app.register_blueprint(bp)
 
 if __name__ == "__main__":
     app.run(
-        #host=os.getenv("IP", "0.0.0.0"), 
+        host=os.getenv("IP", "0.0.0.0"), 
         port=int(os.getenv("PORT", 8080)), debug=True
     )
