1d1f89150ae2b2eb94497259c159c1766c1ef0ae
Melina Phan <mphan13@student.gsu.edu>
2021-12-03 05:49:00 +0000

    final styling changes

--- src/Home.js
+++ src/Home.js
@@ -13,7 +13,7 @@ function Home() {
             <div className="Container">
                 <h2> Meal Planning Made Easy </h2>
                 <p> Are you in need of a weekly meal plan but can't seem to find one that fits you? The Culinary Army allows you browse through tons of weekly meal plan ideas to find one that suits you or you can create your own delicious meals customized to your preference. The possibilities are endless! </p>
-                <br />
+                <br /><br />
                 <div className="Image2">
                     <img className="meals" src={meals} alt="meals"></img>
                 </div>
